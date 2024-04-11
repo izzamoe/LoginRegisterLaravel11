9c7d19a579b17a2c071a79462f34d787321a8f21
Izzamuddin <dimanton1221@gmail.com>
2024-04-11 21:26:27 +0700

    feat: Apply IsLogin middleware to dashboard routes
    This commit applies the `IsLogin` middleware to the dashboard routes in the `web.php` file. With this change, only authenticated users can access the dashboard routes. If a user is not authenticated, they will be redirected to the login page.
    
    Here's a brief overview of the changes:
    
    - The `IsLogin` middleware is applied to the dashboard route group.
    - This affects all routes within the `dashboard` prefix.
    - The `IsLogin` middleware checks if a user is authenticated using Laravel's built-in `Auth::check()` method.
    - If the user is authenticated, the request is allowed to proceed.
    - If the user is not authenticated, the middleware redirects the user to the login page.

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthControllers;
 use App\Http\Controllers\HomeControllers;
+use App\Http\Middleware\IsLogin;
+
 
 Route::get('/', [AuthControllers::class, 'CheckLogin']);
 
@@ -13,7 +15,7 @@ Route::group(['prefix' => '/auth', "as" => "auth."], function () {
     Route::get('/register', [AuthControllers::class, 'RegisterPage'])->name('register');
     Route::post('/register', [AuthControllers::class, 'register']);
 });
-Route::group(['prefix' => "dashboard", "as" => "dashboard."], function () {
+Route::group(['prefix' => "dashboard", "as" => "dashboard.", "middleware" => IsLogin::class], function () {
     Route::get('/', [HomeControllers::class, 'index'])->name('home');
     Route::get('/logout', [AuthControllers::class, 'logout'])->name('logout');
 });
